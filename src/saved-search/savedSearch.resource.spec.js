e7a3aca03dd39e02ed46cdf89f9eef6f68714df1
nebulae <nebulae@gmail.com>
2015-12-09 14:59:23 -0800

    removed configservice, converter, sugar

---
+++ src/saved-search/savedSearch.resource.spec.js
@@ -0,0 +1,28 @@
+describe('savedSearchResource', function () {
+
+  var $http, $q, savedSearchResource;
+
+  beforeEach(function(){
+    // module('vs.tools');
+    // module('vs.tools.savedSearch', []);
+    // module(function ($provide) {
+    //   $provide.constant('config', cfg);
+    // });
+    inject(function (_$http_, _$q_, _savedSearchResource_) {
+      $http = _$http_;
+      $q = _$q_;
+      savedSearchResource = _savedSearchResource_;
+    });
+  });
+
+  // Setup the mock service in an anonymous module.
+  // beforeEach(module(function ($provide) {
+  //   $provide.value('oneOfMyOtherServicesStub', {
+  //       someVariable: 1
+  //   });
+  // }));
+
+  it('can get an instance of my factory', function() {
+    expect(savedSearchResource).toBeDefined();
+  });
+});
--- src/saved-search/savedSearch.resource.ts
+++ src/saved-search/savedSearch.resource.ts
@@ -2,33 +2,13 @@
 declare var config;
 
 angular.module('vs.tools.savedSearch').
-  factory('savedSearchResource', function (sugar, $http, $q, configService, converter) {
+  factory('savedSearchResource', function ($http, $q) {
     'use strict';
 
     function _doSave(request) {
-
-      if (configService.hasChanges()) {
-        var deferred = $q.defer();
-        sugar.postJson(configService.getUpdatedSettings(), 'display', 'config').then(function(response) {
-          request.config = response.data.id;
-          /* jshint ignore:start */
-          request.query += '/disp=' + request.config;
-          request.path = request.query;
-          sugar.postJson(request, 'display', 'ssearch').then(function(savedResponse) {
-            deferred.resolve();
-          }, function(error) {
-            deferred.reject(error);
-          });
-          /* jshint ignore:end */
-        }, function(error) {
-          deferred.reject(error);
-        });
-        return deferred.promise;
-      } else {
-        request.query += '/disp=' + request.config;
-        request.path = request.query;
-        return sugar.postJson(request, 'display', 'ssearch');
-      }
+      request.query += '/disp=' + request.config;
+      request.path = request.query;
+      // return sugar.postJson(request, 'display', 'ssearch');
     }
 
     function _getQueryString() {
@@ -55,11 +35,11 @@ angular.module('vs.tools.savedSearch').
         return _execute();
       },
 
-      saveSearch: function(savedSearch, params) {
-        savedSearch.config = configService.getConfigId();
-        savedSearch.query = converter.toClassicParams(params);
-        return _doSave(savedSearch);
-      },
+      // saveSearch: function(savedSearch, params) {
+      //   savedSearch.config = configService.getConfigId();
+      //   savedSearch.query = converter.toClassicParams(params);
+      //   return _doSave(savedSearch);
+      // },
 
       deleteSearch: function(id){
         return $http.delete(config.root + 'api/rest/display/ssearch/' + id).then(function(){
@@ -81,7 +61,7 @@ angular.module('vs.tools.savedSearch').
         if(afterId !== null) {
           data += 'after=' + afterId;
         }
-        return sugar.postForm('api/rest/display/ssearch/' + id + '/order', data);
+        // return sugar.postForm('api/rest/display/ssearch/' + id + '/order', data);
       }
     };
   });
