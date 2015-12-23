be6ac7ba4ac7c4d2bf30d57ddb2dad148296855b
nebulae <nebulae@gmail.com>
2015-12-23 08:27:38 -0800

    added _savePageConfig to the page config resource

--- src/page-config/page-config-resource.ts
+++ src/page-config/page-config-resource.ts
@@ -3,48 +3,63 @@ declare var config;
 
 angular.module('vs.tools.pageConfig').
 	/* @ngInject */
-  factory('pageConfigResource', function ($http: any) {
+	factory('pageConfigResource', function ($http: any) {
 
-    'use strict';
+		'use strict';
 
-    function _getListQueryString() {
-      var queryString = config.root + 'api/rest/display/config/list';
-      queryString += '?rand=' + Math.random();
-      return queryString;
-    }
+		var configUri = config.root + 'api/rest/display/config/';
 
-    function _getConfigQueryString(id: string) {
-      var queryString = config.root + 'api/rest/display/config/' + id;
-      // queryString += '?rand=' + Math.random();
-      return queryString;
-    }
+		function _getListQueryString() {
+			var queryString = configUri + 'list';
+			queryString += '?rand=' + Math.random();
+			return queryString;
+		}
 
-    function _getPageConfigList() {
-      return $http.get(_getListQueryString()).then(function (data: any) {
-        return data;
-      }, function(error) {
-        // @TODO: handle error
-        console.log(error);
-        return error;
-      });
-    }
+		function _getConfigQueryString(id: string) {
+			var queryString = configUri + id;
+			queryString += '?rand=' + Math.random();
+			return queryString;
+		}
 
-    function _getPageConfig(id: string) {
-      return $http.get(_getConfigQueryString(id)).then(function (data: any) {
-        return data;
-      }, function(error) {
-        // @TODO: handle error
-        console.log(error);
-        return error;
-      });
-    }
+		function _getPageConfigList() {
+			return $http.get(_getListQueryString()).then(function (data: any) {
+				return data;
+			}, function(error: any) {
+				// @TODO: handle error
+				console.log(error);
+				return error;
+			});
+		}
 
-    return {
-      getPageConfigs: function() {
-        return _getPageConfigList();
-      },
-      getPageConfig: function(id: string) {
-        return _getPageConfig(id);
-      }
-    };
-  });
+		function _getPageConfig(id: string) {
+			return $http.get(_getConfigQueryString(id)).then(function (data: any) {
+				return data;
+			}, function(error: any) {
+				// @TODO: handle error
+				console.log(error);
+				return error;
+			});
+		}
+
+		function _savePageConfig(template: any) {
+			return $http.post(configUri, template).then(function (data: any) {
+				return data;
+			}, function(error: any) {
+				// @TODO: handle error
+				console.log(error);
+				return error;
+			});
+		}
+
+		return {
+			getPageConfigs: function() {
+				return _getPageConfigList();
+			},
+			getPageConfig: function(id: string) {
+				return _getPageConfig(id);
+			},
+			savePageConfig: function(template: any){
+				return _savePageConfig(template);
+			}
+		};
+	});
